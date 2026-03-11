17eae3d8e6ec7d5f1ae885b9c371b92cc498e483
eric <ericdu8752@gmail.com>
2026-03-11 23:35:47 +0800

    feat:添加clarity代码

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,6 +66,15 @@ export default function RootLayout({
             gtag('config', 'G-7EN7FET9YX');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="lazyOnload">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vu5pt7a2w9");
+          `}
+        </Script>
       </head>
       <body>
         <ThemeProvider
